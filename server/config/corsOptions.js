c92cdc699845356220ab7a2685548a66ac0f2ac5
Muhammed Kalabaşı <97872051+muhammedkalabasi10@users.noreply.github.com>
2023-03-01 12:49:23 +0300

    cors and readme update
    cors and readme update

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,6 +1,6 @@
 const corsOptions = {
     origin: (origin, callback) => {
-        if (["http://localhost:3006"].indexOf(origin) !== -1 || !origin) {
+        if ([process.env.API_LINK].indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'))
